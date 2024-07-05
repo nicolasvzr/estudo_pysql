5e0308feaee58419fa3836dbfcc2cadef217d93c
Nicolas Vaz <nicolasvaz@outlook.com.br>
2024-07-04 21:21:51 -0300

    Alterei a conexão

--- insert.py
+++ insert.py
@@ -1,12 +1,19 @@
 import mysql.connector
+from config import db_config
+
+db_host = db_config['host']
+db_user = db_config['user']
+db_password = db_config['password']
+db_name = db_config['database']
 
 conexao = mysql.connector.connect(
-    host ='127.0.0.1',
-    user = 'root',
-    password = '12Ngh67LL@',
-    database = 'aula'
+    host = db_host,
+    user = db_user,
+    password = db_password,
+    database = db_name
 )
 
+print("conectei ao banco de dados")
 
 cursor = conexao.cursor()
 
@@ -31,5 +38,5 @@ print("Inserção realizada com sucesso!")
 #     cliente_id, nome, email = resultado
 #     print(f"ID: {cliente_id}, Nome: {nome}, email:{email}")
 
-cursor.close()
+#cursor.close()
 conexao.close()
